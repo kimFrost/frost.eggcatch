044b8ce6fbcbd7d073c9f14bdc47fa3ad66e7053
Kim Frost <kim@noerd.dk>
2015-02-03 13:57:40 +0100

    ()

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,13 +15,26 @@ window.requestAnimFrame = (function () {
   var base = {
     options: {
       debug: true,
-      useRoundNum: true,
-      gravity: 0.05
+      useRoundNum: false,
+      gravity: 0.05,
+      showCollisonModel: true,
+      spawnTicksRequired: 50,
+      playerWidth: 275,
+      playerHeight: 141,
+      eggWidth: 50,
+      eggHeight: 80,
+      playerColumns: 4,
+      playerRows: 2
     },
+    scaleMultiplier: 1,
+    gameTicks: 0,
+    spawnTicks: 0,
     canvas: null,
     canvasCtx: null,
     canvasWidth: 0,
     canvasHeight: 0,
+    lastCanvasWidth: 0,
+    lastCanvasHeight: 0,
     entities: [],
     players: [],
     playerImage: document.createElement('img'),
@@ -41,6 +54,16 @@ window.requestAnimFrame = (function () {
     this.dy = 0;
     this.width = 0;
     this.height = 0;
+    this.slots = [];
+    this.collision = {
+      type: 'box',
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      radius: 0
+    };
+    this.image = base.eggImage;
     return this;
   };
 /**---------------------------------------
@@ -72,13 +95,32 @@ window.requestAnimFrame = (function () {
 ---------------------------------------**/
   function updateLoop() {
     window.requestAnimFrame(updateLoop);
+    checkSpawn();
     update();
     base.fpsCount++;
+    base.gameTicks++;
+    base.spawnTicks++;
   }
 
-  function update() {
-    //base.log(base.entities.length);
-    for (var i=0;i<base.entities.length;i++) {
+  function checkSpawn() {
+    if (base.spawnTicks > base.options.spawnTicksRequired) {
+      var entity = new Entity();
+      entity.x = Math.floor(Math.random() * (base.canvasWidth - 50));
+      entity.height = -(base.options.eggHeight * base.scaleMultiplier);
+      entity.width = base.options.eggWidth * base.scaleMultiplier;
+      entity.height = base.options.eggHeight * base.scaleMultiplier;
+      entity.collision.width = entity.width;
+      entity.collision.height = entity.height;
+      base.entities.push(entity);
+      // Reset spawn ticks
+      base.spawnTicks = 0;
+    }
+  }
+
+  function timedUpdate() {
+    var i,
+      player;
+    for (i=0;i<base.entities.length;i++) {
       var entity = base.entities[i];
       entity.dy += base.options.gravity;
       entity.x += entity.dx;
@@ -87,11 +129,51 @@ window.requestAnimFrame = (function () {
         entity.x = Math.round(entity.x);
         entity.y = Math.round(entity.y);
       }
+      // Check entities for out of screen
       if ((entity.y - entity.height) > base.canvasHeight) {
         base.entities.splice(i, 1);
       }
+      // Collision detect with player
+      else {
+        for (var ii=0;ii<base.players.length;ii++) {
+          player = base.players[ii];
+          if (collisionDetect(player, entity)) {
+            putEgg(player, entity);
+            base.entities.splice(i, 1);
+          }
+        }
+      }
+    }
+  }
+
+  function update() {
+    //base.log(base.entities.length);
+    var i,
+        player;
+    // Update player bounds
+    for (i=0;i<base.players.length;i++) {
+      player = base.players[i];
+      if (player.x < 0) {
+        player.x = 0;
+      }
+      else if (player.x + player.width > base.canvasWidth) {
+        player.x = base.canvasWidth - player.width;
+      }
     }
     draw();
+  }
+
+  function putEgg(player, entity) {
+    for (var i=0; i<player.slots.length; i++) {
+      var slot = player.slots[i];
+      if (slot.entities.length === 0) {
+        slot.entities.push(entity);
+        break;
+      }
+      else {
+        continue;
+      }
+    }
   }
 
   function draw() {
@@ -100,58 +182,109 @@ window.requestAnimFrame = (function () {
     ctx.clearRect(0, 0, base.canvasWidth, base.canvasHeight);
     ctx.save();
     var i;
+    var ii;
     var entity;
-    // Entities
-    for (i=0;i<base.entities.length;i++) {
-      entity = base.entities[i];
-      ctx.drawImage(base.eggImage, entity.x, entity.y, entity.width, entity.height);
-      /*
-      ctx.beginPath();
-      ctx.fillStyle = 'rgba(155, 155, 155, 1)';
-      ctx.arc(entity.x, entity.y, 10, 0, 2*Math.PI, false);
-      ctx.closePath();
-      ctx.fill();
-      */
-    }
+
+    // Players
     for (i=0;i<base.players.length;i++) {
       var player = base.players[i];
       ctx.drawImage(base.playerImage, player.x, player.y, player.width, player.height);
-      //base.log(player);
-      for (var ii=0;ii<base.entities.length;ii++) {
-        entity = base.entities[ii];
-        if (collisionDetect(player, entity)) {
-          base.entities.splice(ii, 1);
+      if (base.options.showCollisonModel) {
+        ctx.fillStyle = 'rgba(150,0,0,0.3)';
+        ctx.fillRect(player.x + player.collision.x, player.y + player.collision.y, player.collision.width, player.collision.height);
+      }
+      // Entities stored in player slots
+      for (ii = 0; ii < player.slots.length; ii++) {
+        var slot = player.slots[ii];
+        for (var iii = 0; iii < slot.entities.length; iii++) {
+          entity = slot.entities[iii];
+
+          ctx.drawImage(entity.image, player.x + slot.ix * entity.width, player.y + slot.iy * entity.height, entity.width, entity.height);
         }
       }
     }
+
+    // Entities
+    for (i=0;i<base.entities.length;i++) {
+      entity = base.entities[i];
+      ctx.drawImage(entity.image, entity.x, entity.y, entity.width, entity.height);
+      if (base.options.showCollisonModel) {
+        ctx.fillStyle = 'rgba(150,0,0,0.3)';
+        ctx.fillRect(entity.x + entity.collision.x, entity.y + entity.collision.y, entity.collision.width, entity.collision.height);
+      }
+    }
     // Fps
+    ctx.fillStyle = 'rgb(0,0,0)';
     ctx.font='20px Georgia';
-    ctx.fillText(base.fps,10,30);
+    ctx.fillText('FPS: ' + base.fps,10,30);
+    ctx.fillText('TICKS: ' + base.gameTicks,10,50);
+    ctx.fillText('EGGS: ' + base.entities.length, 10,70);
   }
 /**---------------------------------------
   Collision Detection
 ---------------------------------------**/
   var collisionDetect = function(c1, c2, type) {
     type = (type === undefined) ? 'box' : type;
+    var x1 = c1.x + c1.collision.x;
+    var y1 = c1.y + c1.collision.y;
+    var x2 = c2.x + c2.collision.x;
+    var y2 = c2.y + c2.collision.y;
     if (type === 'box') {
-      return (c1.x < c2.x + c2.width &&
-      c1.x + c1.width > c2.x &&
-      c1.y < c2.y + c2.height &&
-      c1.height + c1.y > c2.y);
+      return (x1 < x2 + c2.collision.width &&
+      x1 + c1.collision.width > x2 &&
+      y1 < y2 + c2.collision.height &&
+      c1.collision.height + y1 > y2);
     }
     else if (type === 'circle') {
-      var dx = c1.x - c2.x;
-      var dy = c1.y - c2.y;
-      var dist = c1.radius + c2.radius;
+      var dx = x1 - x2;
+      var dy = y1 - y2;
+      var dist = c1.collision.radius + c2.collision.radius;
       return (dx * dx + dy * dy <= dist * dist);
     }
   };
 /**---------------------------------------
   Resize
 ---------------------------------------**/
+  var resize = function(width, height) {
+    base.canvas.width = width;
+    base.canvas.height= height;
+    recal();
+  };
   var recal = function() {
     base.canvasWidth = base.canvas.width;
     base.canvasHeight = base.canvas.height;
+    var widthRatio = base.canvasWidth / base.lastCanvasWidth;
+    // Set Size Profile
+    if (base.canvasWidth > 1050) {
+      base.scaleMultiplier = 1;
+    }
+    else if (base.canvasWidth > 600) {
+      base.scaleMultiplier = 0.7;
+    }
+    else {
+      base.scaleMultiplier = 0.4;
+    }
+    // Set player size and position
+    base.players[0].x = base.players[0].x * widthRatio;
+    base.players[0].y = base.canvasHeight - (200 * base.scaleMultiplier);
+    base.players[0].width = base.options.playerWidth * base.scaleMultiplier;
+    base.players[0].height = base.options.playerHeight * base.scaleMultiplier;
+    base.players[0].collision.y = 80 * base.scaleMultiplier;
+    base.players[0].collision.width = base.players[0].width;
+    base.players[0].collision.height = 20 * base.scaleMultiplier;
+
+    for (var i=0;i<base.entities.length;i++) {
+      var entity = base.entities[i];
+      entity.x = entity.x * widthRatio;
+      entity.width = base.options.eggWidth * base.scaleMultiplier;
+      entity.height = base.options.eggHeight * base.scaleMultiplier;
+      entity.collision.width = entity.width;
+      entity.collision.height = entity.height;
+    }
+
+    // Store canvas sizes for ratio calculations
+    base.lastCanvasWidth = base.canvasWidth;
+    base.lastCanvasHeight = base.canvasHeight;
   };
 /**---------------------------------------
   Bindings
@@ -170,27 +303,27 @@ window.requestAnimFrame = (function () {
       //base.log('touchend');
     });
     base.canvas.addEventListener('mousemove', function(event) {
-      base.log('mousemove', event);
+      //base.log('mousemove', event);
       base.players[0].x = event.x - (base.players[0].width / 2);
     });
     base.canvas.addEventListener('touchmove', function(event) {
-      base.log('touchmove', event);
+      //base.log('touchmove', event);
+      event.preventDefault();
       base.players[0].x = event.touches[0].clientX - (base.players[0].width / 2);
+    });
+    // Resize
+    window.addEventListener('resize', function(event){
+      // Update canvas size
+      var parent = base.canvas.parentElement;
+      resize(parent.clientWidth, parent.clientHeight);
     });
   };
 /**---------------------------------------
   Start
 ---------------------------------------**/
   var start = function() {
-    setInterval(function() {
-      var entity = new Entity();
-      entity.x = Math.floor(Math.random() * base.canvasWidth);
-      entity.y = -80;
-      entity.width = 50;
-      entity.height = 80;
-      base.entities.push(entity);
-    }, 20);
     updateLoop();
+    setInterval(timedUpdate, 1000 / 60);
   };
 /**---------------------------------------
   Initialize
@@ -209,18 +342,34 @@ window.requestAnimFrame = (function () {
     base.canvas = canvas;
     base.canvasCtx = canvas.getContext('2d');
     base.eggImage.src = 'egg.png';
-    base.playerImage.src = 'paddle.png';
-
-    // Update canvas size
-    recal();
+    base.playerImage.src = 'bakke.png';
 
     // Create Paddle
     var player = new Entity();
-    player.x = Math.floor(0.5 * base.canvasWidth - 100);
-    player.y = base.canvasHeight - 100;
-    player.width = 200;
-    player.height = 60;
+    player.x = Math.floor(0.5 * base.canvasWidth - (base.options.playerWidth / 2));
+    player.y = base.canvasHeight - 200;
+    player.width = base.options.playerWidth;
+    player.height = base.options.playerHeight;
+    player.collision.type = 'box';
+    player.collision.x = 0;
+    player.collision.y = 80;
+    player.collision.width = player.width;
+    player.collision.height = 20;
+
+    // Create player slot for eggs
+    for (var i=0; i < base.options.playerColumns * base.options.playerRows; i++) {
+      player.slots.push({
+        ix: i % 4,
+        iy: Math.floor(i / 4),
+        entities: []
+      });
+    }
+
     base.players.push(player);
+
+    // Update canvas size
+    var parent = base.canvas.parentElement;
+    resize(parent.clientWidth, parent.clientHeight);
 
     setupBindings();
     start();
